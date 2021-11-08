1e4d0bd7aff551ee0e48f9a3981718fa07059716
NTatarnikov <t4t4rn1k0v@gmail.com>
2021-11-08 19:05:53 +0300

    Initial Commit

---
+++ SwiftUIGuessTheFlag/SwiftUIGuessTheFlagApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIGuessTheFlagApp.swift
+//  SwiftUIGuessTheFlag
+//
+//  Created by Никита on 08.11.2021.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIGuessTheFlagApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
